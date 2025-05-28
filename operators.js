38b53fc5789a0859bfb6cd92ced5c53ea1ac7461
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-28 21:54:22 +0530

    liberal equality and inequality example added

--- operators.js
+++ operators.js
@@ -2,3 +2,6 @@ let string12 = "10";
 let num = 10;
 console.log("Equal == : ",string12 == num); //liberal equality
 console.log("Equal === : ", string12 === num);//strict equality that includes type also
+
+console.log(string12!=num); // liberal inequality
+console.log(string12!==num); // strict inequality
