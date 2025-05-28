037b1aa35f6c287c64c93b5d0a3a15d6e3a8ff69
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-28 21:49:59 +0530

    comparison operator example added

---
+++ operators.js
@@ -0,0 +1,4 @@
+let string12 = "10";
+let num = 10;
+console.log("Equal == : ",string12 == num); //liberal equality
+console.log("Equal === : ", string12 === num);//strict equality that includes type also
