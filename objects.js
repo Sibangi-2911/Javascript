2f04c66792561a133d6c7063718f7fd4732178bf
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-29 22:57:10 +0530

    added  example

---
+++ objects.js
@@ -0,0 +1,16 @@
+let car = {
+  brand:"BMW",
+  model : "7 Series",
+  color : "white",
+  make : 2028,
+  "car-owner": "Sibangi",
+};
+console.log(car);
+console.log(car["brand"]);
+console.log(car.model);
+console.log(car["car-owner"])
+
+
+car.color = "black";
+console.log(car["color"]);
+
