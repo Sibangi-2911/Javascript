138f9d23a0c3d0c0e96fb441964bf1666f4c310e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-29 23:04:42 +0530

    object creation using constructor function added

--- objects.js
+++ objects.js
@@ -14,3 +14,10 @@ console.log(car["car-owner"])
 car.color = "black";
 console.log(car["color"]);
 
+//constructor function- another way to create object
+let audi = new Object();
+console.log(audi);
+audi.brand = "Audi";
+audi.color = "red";
+audi.oWner = "Sibangi";
+console.log(audi);
