0016674b758c840229bf043373abceb9b3dd3092
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-31 11:49:11 +0530

    another array syntax and spread operator use added

---
+++ array1.js
@@ -0,0 +1,16 @@
+// ALTERNATIVE SYNTAX FOR CREATING AN ARRAY
+let numbers1 = new Array(12,14,56,34,67);
+console.log(numbers1);
+numbers1.push(30);
+console.log(numbers1);
+
+let newnumbers = numbers1;
+numbers1.push(1);
+console.log(newnumbers); // mutable
+console.log(numbers1);
+
+// avoid mutability by using spread operator
+let newnumbers1 = [...numbers1];
+numbers1.push(69);
+console.log(numbers1);
+console.log(newnumbers1);
