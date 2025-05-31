eea9238534739199f35d5e74777fdaabc2007680
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-31 11:25:44 +0530

    array examples added

---
+++ array.js
@@ -0,0 +1,13 @@
+// Array examples
+let fruits = ["Apple","Banana","Strawberry","Mango","Grapes"]
+console.log(fruits);
+console.log(fruits[0]);
+
+let numbers = [1,2,3,4,5];
+console.log(numbers);
+console.log(numbers[4]);
+
+let mixedarray = [null, true, "Hello", 30, {name:"Sibangi"},[2,4,6]];
+console.log(mixedarray);
+console.log(mixedarray[4]);
+console.log(mixedarray[4]["name"]);
