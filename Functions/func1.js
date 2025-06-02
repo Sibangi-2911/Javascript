4e87f4703654e90409794d907fe2cec1fe48347b
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-02 22:31:39 +0530

    function declaration, function expression and arrow function added

---
+++ Functions/func1.js
@@ -0,0 +1,25 @@
+function addNumbers(num1, num2){
+  return num1+num2;
+}
+let result = addNumbers(5,10);
+console.log(result);
+
+function add(){
+  console.log("Add Body");
+}
+add();
+
+//Function expressions - functions assigned to a variable. These are not hoisted.
+const saygoodbye = function(name){
+  console.log(`Goodbye ${name}`);
+}
+saygoodbye("Sibangi");
+
+//Arrow functions - don not have this context and not hoisted
+const add1 = (a,b)=>{
+  return a+b;
+}
+console.log(add1(10,20));
+
+const multi = (a,b)=>a*b;
+console.log(multi(8,2));
