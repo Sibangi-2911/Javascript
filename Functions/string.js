71f58dded7abede5ebd31388ed8bccd73496327d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-03 23:22:12 +0530

    added string methods example

---
+++ Functions/string.js
@@ -0,0 +1,6 @@
+let example = "Javascript is awesome!!!";
+console.log(example.length);
+console.log(example.toLowerCase());
+console.log(example.toUpperCase());
+console.log(example.charAt(5));
+console.log(example.indexOf("awesome"));
