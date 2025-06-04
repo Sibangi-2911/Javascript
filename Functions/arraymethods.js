4fceb80f307d8188ea1c0a724437ba9630e05a19
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-04 11:52:05 +0530

    added array methods

---
+++ Functions/arraymethods.js
@@ -0,0 +1,16 @@
+let fruits = ["Banana","Apple","Mango"];
+fruits.push("Pineapple");
+console.log(fruits);
+
+fruits.pop()
+console.log(fruits);
+
+fruits.unshift("Blueberry");
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+console.log(fruits.indexOf("Apple"));
+
+console.log(fruits.includes("Mango"));
