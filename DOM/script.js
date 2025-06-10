7b6da11176b8f14b88a3b1d0acc95d122a076aa0
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-10 22:43:17 +0530

    selecting elements by class name

--- DOM/script.js
+++ DOM/script.js
@@ -4,3 +4,9 @@
 let headings = document.getElementById("main-title");
 console.log(headings);
 console.log(headings.textContent);
+
+//select elements by class
+let classes = document.getElementsByClassName("content");
+console.log(classes);
+console.log(classes.length);
+console.log(classes[0]);
