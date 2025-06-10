56d90ab0d48bb73cf3be2b66a23e63449b7c595f
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-10 22:40:12 +0530

    selecting elements by id

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+//SELECTING DOM ELEMENTS USING SELECTORS
+
+//Select element with id
+let headings = document.getElementById("main-title");
+console.log(headings);
+console.log(headings.textContent);
