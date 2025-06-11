4ed76e020f2596c49743977684cd42b5e0d4d631
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-11 19:44:37 +0530

    selecting the required elements using query selector and queryselectorall

---
+++ DOM/scriptquery.js
@@ -0,0 +1,24 @@
+let firstheading = document.querySelector("h1");
+let allheadings = document.querySelectorAll("h1"); 
+console.log(firstheading.textContent);
+console.log(allheadings);
+
+let firstpara = document.querySelector(".intro");
+let allparas = document.querySelectorAll(".intro");
+console.log(firstpara.textContent);
+console.log(allparas);
+
+let firstitem = document.querySelector("#list li");
+let allitem = document.querySelectorAll("#list li");
+console.log(firstitem.textContent);
+console.log(allitem);
+
+let firtsbutton = document.querySelector("button");
+let allbutton = document.querySelectorAll("button");
+console.log(firtsbutton);
+console.log(allbutton);
+
+let firstnav = document.querySelector("nav");
+let allnav = document.querySelectorAll("nav");
+console.log(firstnav.textContent);
+console.log(allnav);
