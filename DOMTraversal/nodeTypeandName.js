42d73be5914e23e345ec524d62b9dd4e62c98ac3
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-14 16:26:28 +0530

    node name and node type

---
+++ DOMTraversal/nodeTypeandName.js
@@ -0,0 +1,9 @@
+//node type and node name
+let divElement = document.getElementById("sample-element");
+console.log(divElement.nodeName);
+console.log(divElement.nodeType);
+
+let textNode = divElement.firstChild;
+console.log(textNode);
+console.log(textNode.nodeName);
+console.log(textNode.nodeType);
