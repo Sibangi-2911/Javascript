246d1055e06be1279fdabd85804d40939b7a433d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-14 16:23:35 +0530

    selection of first child and last child both element and node

---
+++ DOMTraversal/firstlastchild.js
@@ -0,0 +1,13 @@
+//selecting first child and last child
+let parent = document.getElementById("parent");
+
+let firstNode = parent.firstChild;
+console.log(firstNode);
+let lastNode = parent.lastChild;
+console.log(lastNode);
+
+let firstChildElement = parent.firstElementChild;
+console.log(firstChildElement);
+
+let lastElementChild = parent.lastElementChild;
+console.log(lastElementChild);
