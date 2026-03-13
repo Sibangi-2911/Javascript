7a2d1a0bd87bad6d6eaa83d45d0c200b7769002b
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-13 15:43:09 +0530

    String and boolean datatype declaration done

--- Practice/basics1.js
+++ Practice/basics1.js
@@ -8,6 +8,16 @@ console.log(typeof a); //type of datatype
 let b = 234.6;
 console.log(typeof b);
 
+//String declaration
+let name = "Sibangi Boxipatro";
+console.log(typeof name);
+
+//boolean declaration
+let required = true;
+console.log(typeof required);
+
+//null and undefined datatypes are also there
+
 // these are single line comments
 /*
 Multi 
