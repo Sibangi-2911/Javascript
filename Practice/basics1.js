7107ca74eda16b170909590e8669e7aeca683c5d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-13 15:38:54 +0530

    Introduction to js, print statement, comments & declaration of Number variable done

---
+++ Practice/basics1.js
@@ -0,0 +1,16 @@
+console.log("Hello World!!!");
+
+//declare  Number variable
+let a = 4;
+console.log(a);
+console.log(typeof a); //type of datatype
+
+let b = 234.6;
+console.log(typeof b);
+
+// these are single line comments
+/*
+Multi 
+line 
+comments
+*/
