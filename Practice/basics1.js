87d91f5c54c730f8d8aaa83996a57ba0db4877a9
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-13 15:51:30 +0530

    Completed variable declaration concept

--- Practice/basics1.js
+++ Practice/basics1.js
@@ -9,14 +9,19 @@ let b = 234.6;
 console.log(typeof b);
 
 //String declaration
-let name = "Sibangi Boxipatro";
-console.log(typeof name);
+var c = "Sibangi Boxipatro";
+console.log(typeof c);
 
 //boolean declaration
 let required = true;
 console.log(typeof required);
+console.log(!required); // reversed value to false
 
 //null and undefined datatypes are also there
+
+//let c = a+b , we cannot redeclare variable with let keyword but possible with var. But reassigning can be done with let keyword.
+var c = a + b;
+console.log(c);
 
 // these are single line comments
 /*
