d0af08c6caae6a921dd1573bc093c238a06d3598
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-26 19:56:56 +0530

    Modified the comment

--- Practice/basics9.js
+++ Practice/basics9.js
@@ -1,4 +1,4 @@
-//Inheritance--- Main pillar in OOPs
+//Inheritance--- is the main pillar in OOPs
 //one class can inherit/acquire the properties ,methods of another class
 //The class which inherits the properties of other is known as subclass/derived class/child class
 //The class whose properties are inherited is known as superclass/parent class
